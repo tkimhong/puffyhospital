1510dfe7c263caf63186b0cc85d6606d80217359
Crastyz <Hoeutiangkimhong@gmail.com>
2024-11-15 18:53:16 +0700

    Set up React app with Vercel Analytics and redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        // Redirect immediately after rendering
+        window.location.href = "https://www.youtube.com/watch?v=twQ3kkeZ6Fk";
+    }, []);
+
+    return (
+        <div>
+            <p>Redirecting... If you are not redirected, <a href="https://www.youtube.com/watch?v=twQ3kkeZ6Fk">click here</a>.</p>
+            <Analytics />
+        </div>
+    );
 }
 
 export default App;
